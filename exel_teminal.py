f032f0f62d2a956a38012813f400dc2e6ac963df
Rafael Koti <rafaelsugaikoti404@gmail.com>
2026-09-05 18:01:38 -0300

    Adicionando comando para ler as planilhas (Pandas)

--- exel_teminal.py
+++ exel_teminal.py
@@ -1,9 +1,14 @@
 # OpenpyXL e Pandas são as bibliotecas que vamo usar para ler dados das planilhas do Exel (aparentemente elas são as principais)
 
-import openpyxl, pandas
+import openpyxl
+import pandas as pd
 
-# Como criar as planilhas? - com essa biblioteca OpenpyXL, vamo usar o WorkBook para criar as nossas planilhas
+'''Como criar as planilhas? - com essa biblioteca OpenpyXL, vamo usar o WorkBook para criar as nossas planilhas'''
 criar_planilha = openpyxl.workbook()
 
+'''Como vamos ler os arquivos Exel? - Vamos usar o Pandas (PD, pois está como abreviação da biblioteca'''
 
+''' No parênteses vamos colocar o nome da planilha que o usuário quiser ler, mais pra frente da pra fazer um códifo onde a
+váriavel mesmo lê qual arquivo a pessoa digitou'''
 
+ler_planilhas = pd.read_excel()
