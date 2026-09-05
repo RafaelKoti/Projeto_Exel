0bc58a003b7c24292709d80c816da6458bc2aff5
Rafael Koti <rafaelsugaikoti404@gmail.com>
2026-09-05 17:45:06 -0300

    criando código para criar as planilhas do exel

--- exel_teminal.py
+++ exel_teminal.py
@@ -1,1 +1,9 @@
+# OpenpyXL e Pandas são as bibliotecas que vamo usar para ler dados das planilhas do Exel (aparentemente elas são as principais)
+
 import openpyxl, pandas
+
+# Como criar as planilhas? - com essa biblioteca OpenpyXL, vamo usar o WorkBook para criar as nossas planilhas
+criar_planilha = openpyxl.workbook()
+
+
+
